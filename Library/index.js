ef16c6cf553b7f5d7014c39bfe99d386223b9e14
nungchucks <paunung04@gmail.com>
2022-12-17 00:16:08 -0500

    Currently on: Resetting placeholders

--- Library/index.js
+++ Library/index.js
@@ -1,7 +1,12 @@
 let myLibrary = [];
 const addButton = document.querySelector('.addBookButton');
-const addContainer = document.querySelector('.addBookContainer')
-const submitButton = document.querySelector('.submitButton')
+const addContainer = document.querySelector('.addBookContainer'); 
+const submitButton = document.querySelector('.submitButton'); 
+let author = document.getElementById('addAuthor'); 
+let pages = document.getElementById('addPages'); 
+let title = document.getElementById('addTitle'); 
+let read = document.getElementById('readIt'); 
+
 const Book = (author, title, pages) => {
 
 }
@@ -16,17 +21,18 @@ const displayBooks = () => {
 
 addButton.addEventListener('click', () => {
     addContainer.style.visibility = 'visible'
+    if (read.checked) {
+        read.checked = false; 
+    }
+    author.placeholder="your message";
+
 })
 
 submitButton.addEventListener('click', () => {
     addContainer.style.visibility = 'hidden'
+    author.value = ' '
+    pages.value = ' '
+    title.value = ' '
+
 })
 
-// document.body.onclick = function(e){
-//       addBookContainer.style.visibility = 'hidden';
-// }
-
-
-
-const height = document.body.scrollHeight; 
-console.log(height); 
