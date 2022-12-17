2b8e634929818f9e9a2c834bccbc18758bf39ae4
nungchucks <paunung04@gmail.com>
2022-12-17 16:20:10 -0500

    Adding read/not read functions

--- Library/index.js
+++ Library/index.js
@@ -42,17 +42,45 @@ const addBook = () => {
       // Create a new book card element
       const bookCard = document.createElement('div');
       bookCard.classList.add('book');
+      bookCard.style="padding-top: 5px; padding-right: 10px; padding-bottom: -50px; padding-left: 20px;"
   
       // Set the book card's content to the book's title, author, and page count
-      bookCard.innerHTML = `
+      if (book.read) {
+        bookCard.innerHTML = `
+          <h2>${book.title}</h2>
+          <p>By ${book.author}</p>
+          <p>${book.pages} pages</p>
+          <button type="button" style="background-color: #56a66b; border-radius: 10px; width:100px; margin: auto;">Read</button>`
+        }
+      else {
+        bookCard.innerHTML = `
         <h2>${book.title}</h2>
         <p>By ${book.author}</p>
-        <p>${book.pages} pages</p>`;
+        <p>${book.pages} pages</p>
+        <button type="button" style="background-color: #d14f4f; border-radius: 10px; width:100px; margin: auto;">Not read</button>`
+      }
   
       // Append the book card to the booksContainer element
       booksContainer.appendChild(bookCard);
     }
   }
+
+  const readButtons = document.querySelectorAll('.book .read-button');
+
+  readButtons.forEach(button => {
+    button.addEventListener('click', event => {
+      // Get the book element that the button belongs to
+      const bookElement = event.target.closest('.book');
+      // Get the index of the book in the myLibrary array
+      const bookIndex = myLibrary.findIndex(book => book.title === bookElement.querySelector('h2').textContent);
+      // Toggle the read status of the book
+      myLibrary[bookIndex].read = !myLibrary[bookIndex].read;
+      // Update the book card's display
+      displayBooks();
+    });
+  });
+  
+
 
 addButton.addEventListener('click', () => {
     addContainer.style.visibility = 'visible'
