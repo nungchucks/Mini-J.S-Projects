568700022ea61323e16b426da54b3ad3d172f5e2
nungchucks <paunung04@gmail.com>
2022-12-20 21:52:08 -0500

    Final

--- TicTacToe/index.js
+++ TicTacToe/index.js
@@ -1,4 +1,15 @@
 const cell = document.querySelectorAll('.cell');
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+let movesMade = 0; 
 
 let playerFactory = (name, character) => {
     return {name, character}
@@ -7,16 +18,39 @@ let playerFactory = (name, character) => {
 let playerOne = playerFactory('Player One', 'X');
 let playerTwo = playerFactory('Player Two', 'O');
 
-document.addEventListener('click', function(e) {
-    console.log(e.target);
-})
-
 const playGame = () => {
     let currentPlayer = playerOne;
     cell.forEach(cell => {
         cell.addEventListener('click', function(e) {
-            e.target.innerHTMl = currentPlayer.character;
+            e.target.textContent = currentPlayer.character;
             currentPlayer = currentPlayer === playerOne ? playerTwo : playerOne;
+            checkWinner(); 
         })
     })
 }
+
+const checkWinner = () => {
+  movesMade++;
+  if (movesMade < 5) {
+    return;
+  }
+  let winner = null;
+  winningCombinations.forEach(combination => {
+    const cell1 = cell[combination[0]];
+    const cell2 = cell[combination[1]];
+    const cell3 = cell[combination[2]];
+    if (cell1.textContent === cell2.textContent && cell2.textContent === cell3.textContent) {
+      winner = cell1.textContent;
+    }
+  });
+  if (winner) {
+    alert(`${winner} has won!`);
+  } else if (movesMade === 9) {
+    alert('The game is a tie!');
+  }
+};
+
+
+
+
+playGame();
